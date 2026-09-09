8515335770bbfa844476934b729ba876324483b8
Sreekesh OS <sreekeshokky@gmail.com>
2026-09-09 19:11:42 +0530

    Refactor: add @vercel/analytics dependency and integrate analytics component in main application

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import App from "./App";
 import "./styles.css";
 import { applyTheme } from "./lib/theme";
@@ -10,5 +11,6 @@ applyTheme();
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
+    <Analytics />
   </BrowserRouter>,
 );
